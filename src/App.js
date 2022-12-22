de8fd74c05a9d3458b016fdb173eef58237b00a3
KYTNICK <kytnickv@gmail.com>
2022-12-22 14:53:28 +0200

    start to learn React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+import PostItem from "./components/PostItem";
+import "./styles/App.css";
 
 function App() {
+  const [value, setValue] = useState("text in input");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PostItem />
+      <PostItem />
+      <PostItem />
+      <PostItem />
+      <PostItem />
     </div>
   );
 }
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+const PostItem = () => {
+    return (
+        <div className="post">
+        <div className="pots__content">
+          <strong>1. Javascript</strong>
+          <div>JS - programing language</div>
+        </div>
+        <div className="post__btns">
+          <button>Delete</button>
+        </div>
+      </div>
+    )
+}
+export default PostItem;
