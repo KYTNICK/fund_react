2c8ce2fb222bb09c63308a9bbc6f2186341e6354
KYTNICK <kytnickv@gmail.com>
2022-12-24 12:54:28 +0200

    create PostForm

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 import "./styles/App.css";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
 
 function App() {
-  const [value, setValue] = useState("text in input");
-
+  const [posts, setPosts] = useState([
+    { id: 1, title: "Javascript", body: "start to learn JS" },
+    { id: 2, title: "Javascript 2", body: "start to learn JS" },
+    { id: 3, title: "Javascript 3", body: "start to learn JS" },
+  ]);
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+  };
   return (
     <div className="App">
-      <PostItem />
-      <PostItem />
-      <PostItem />
-      <PostItem />
-      <PostItem />
+      <PostForm create={createPost} />
+      <PostList posts={posts} title="Posts about JS" />
     </div>
   );
 }
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-const PostItem = () => {
+const PostItem = (props) => {
     return (
         <div className="post">
         <div className="pots__content">
-          <strong>1. Javascript</strong>
-          <div>JS - programing language</div>
+          <strong>{props.number}. {props.post.title}</strong>
+          <div>{props.post.body}</div>
         </div>
         <div className="post__btns">
           <button>Delete</button>
