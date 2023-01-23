36d43f0fc391ac8bf20bb9e8eea0ac3f34eee9c6
KYTNICK <kytnickv@gmail.com>
2023-01-23 16:32:23 +0200

    add new modal with "PostForm"

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
 import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/modal/MyModal";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -17,7 +18,7 @@ function App() {
     { id: 3, title: "Javascript 3", body: "start to learn JS" },
   ]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
-
+  const [modal, setModal] = useState(false);
   //============
 
   const sortedPosts = useMemo(() => {
@@ -38,13 +39,21 @@ function App() {
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
+    setModal(false);
   };
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
+
+  //======================
   return (
     <div className="App">
-      <PostForm create={createPost} />
+      <MyButton style={{ marginTop: "25px" }} onClick={() => setModal(true)}>
+        Create new Post
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
       <hr style={{ margin: "15px 0" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
 
--- src/components/UI/modal/MyModal.jsx
+++ src/components/UI/modal/MyModal.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import classes from './MyModal.module.css';
+const MyModal = ({children, visible, setVisible}) => {
 
-function MyModal() {
+    const rootClasses = [classes.myModal]
+    if(visible) {
+        rootClasses.push(classes.active);
+    }
   return (
-    <div>MyModal</div>
-  )
-}
+    <div className={rootClasses.join(' ')} onClick={() => setVisible(false)}>
+        <div className={classes.myModalContent} onClick={(e) => e.stopPropagation()}>
+            {children}
+        </div>
+    </div>
+  );
+};
 
 export default MyModal;
