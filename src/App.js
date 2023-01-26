1ad00d06dd236649be957edbf84d8f8bd00bd2db
KYTNICK <kytnickv@gmail.com>
2023-01-27 00:39:31 +0200

    add page list and some feature

--- src/App.js
+++ src/App.js
@@ -8,13 +8,26 @@ import MyModal from "./components/UI/modal/MyModal";
 import { usePosts } from "./hooks/usePosts";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/loader/Loader";
-
+import { useFetching } from "./hooks/useFetching";
+import { getPageCount, getPagesArray } from "./components/utils/pages";
 function App() {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [isPostsLoading, setIsPostsLoading] = useState(false);
+  let pagesArray = getPagesArray(totalPages);
+  console.log(pagesArray);
+
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data);
+    const totalCount = response.headers["x-total-count"];
+
+    setTotalPages(getPageCount(totalCount, limit));
+  });
 
   useEffect(() => {
     fetchPosts();
@@ -27,23 +40,19 @@ function App() {
     setModal(false);
   };
 
-  async function fetchPosts() {
-    setIsPostsLoading(true);
-    setTimeout(async () => {
-      const posts = await PostService.getAll();
-      setPosts(posts);
-      setIsPostsLoading(false);
-    }, 5000);
-  }
-
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
+  };
+
+  const changePage = (page) => {
+    setPage(page);
+    fetchPosts();
   };
 
   //======================
   return (
     <div className="app">
-      <MyButton style={{ marginTop: "25px" }} onClick={() => setModal(true)}>
+      <MyButton style={{ marginTop: "50px" }} onClick={() => setModal(true)}>
         Create new Post
       </MyButton>
       <MyModal visible={modal} setVisible={setModal}>
@@ -51,6 +60,7 @@ function App() {
       </MyModal>
       <hr style={{ margin: "15px 0" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
+      {postError && <h1>Something went wrong ${postError}</h1>}
       {isPostsLoading ? (
         <div
           style={{
@@ -68,6 +78,17 @@ function App() {
           title="Posts about JS"
         />
       )}
+      <div className="page__wrapper">
+        {pagesArray.map((p) => (
+          <span
+            onClick={() => changePage(p)}
+            key={p}
+            className={page === p ? "page page__current" : "page"}
+          >
+            {p}
+          </span>
+        ))}
+      </div>
     </div>
   );
 }
